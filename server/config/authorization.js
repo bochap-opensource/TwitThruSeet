e15c716fb3104e38a00bdcb7b97690b70b7f622c
Darren Seet Serkai <darren@seet.me>
2014-06-16 09:48:48 +0900

    Branching for Heroku

---
+++ server/config/authorization.js
@@ -0,0 +1,13 @@
+/**
+ * Created by dseet on 6/11/2014.
+ */
+/**
+ * Created by dseet on 6/9/2014.
+ */
+module.exports = {
+    'twitterAuth': {
+        'consumerKey': 'wfb64TLhjdf2qQSYoy9HCAHS3',
+        'consumerSecret': '94xgtDGmFBCG3K0fF9wUnlyW64nMPd3FtIqxLOoP1ysgIEyA2R',
+        'callbackURL': 'http://127.0.0.1:3000/auth/twitter/callback'
+    }
+};
